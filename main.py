03d89037e736a106e89d14f3565b6fa26c05dbf6
Holdmedve <talos.boti@gmail.com>
2021-02-05 22:18:48 +0100

    introduce Frame class, expand Mozavid class

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from process_vid import CreateFrames
+from process_vid import Mozavid
 
+# setup
 video_path = 'RICE_BALLS.mp4'
-CreateFrames(video_path)
+target_frame = 360
+
+dst_path = 'my_mozaic'
+recursion_level = 4 # resolution = 4^recursion_level
+
+mozavid = Mozavid(video_path, target_frame, recursion_level)
+mozavid.CreateMozaic(dst_path)
--- process_vid.py
+++ process_vid.py
@@ -1,28 +1,47 @@
 import numpy as np
 import cv2
 
-def CreateFrames(vid_path):
-    vidcap = cv2.VideoCapture(vid_path)
+class Frame:
 
-    success, frame = vidcap.read()
-    prev_frame = frame
-    success, frame = vidcap.read()    
+    def __init__(self, image):
+        self.hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    count = 0
-    limit = 500
-    step = 12 # number of frames to step over
+        
+    
 
-    while success and count < limit:
-        if count % step == 0:
-            cv2.imwrite("frames/frame%d.jpg" % count, frame)  # save frame as JPEG file  
-            print('still chewing...')    
+class Mozavid:
+    def __init__(self, vid_path, target_frame, recursion_level):
+        self.vidcap = cv2.VideoCapture(vid_path)
+        self.target_frame = target_frame
+        self.recursion_level = recursion_level
 
-        prev_frame = frame
-        success, frame = vidcap.read()
+    def CreateMozaic(self, dst_path):
+        self.dst_path = dst_path # path of the final result
 
-        count += 1
+        # in case you need it
+        #framespersecond= int(cap.get(cv2.CAP_PROP_FPS))
 
-def FramesAreDifferent(one_frame, other_frame):
-    #one_frame.shape == other_frame.shape and not(np.bitwise_xor(one_frame,other_frame).any())
-    # here i assume that the two frames have the same shape
-    return np.bitwise_xor(one_frame,other_frame).any()
+        success, image = self.vidcap.read()
+        count = 0
+        frames = []
+
+        while success:
+            if count % 100 == 0:
+                # cv2.imwrite("frames/frame%d.jpg" % count, frame)  # save frame as JPEG file
+                print('still chewing...')                
+
+            frame = Frame(image)
+            frames.append(frame)
+
+            success, image = self.vidcap.read()
+            print(type(image))
+            count += 1
+    
+    def ExecuteHistogramComparison():
+        pass
+
+    
+    def FramesAreDifferent(self, one_frame, other_frame):
+        #one_frame.shape == other_frame.shape and not(np.bitwise_xor(one_frame,other_frame).any())
+        # here i assume that the two frames have the same shape
+        return np.bitwise_xor(one_frame,other_frame).any()
