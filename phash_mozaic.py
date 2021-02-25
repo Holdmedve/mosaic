d7a8bdf9ff2634d97a19004f2ea3d1ca0e774375
Holdmedve <talos.boti@gmail.com>
2021-02-25 15:38:03 +0100

    calculate hash for each image channel

--- phash_mozaic.py
+++ phash_mozaic.py
@@ -1,26 +1,28 @@
 import cv2
 
+def diffHash(image, hashSize=8):    
+    # width is increased because the goal is to create
+    # a diff image with dimensions hashSize*hashSize 
+    # where the diff comes from adjacent columns
+    resized = cv2.resize(image, (hashSize + 1, hashSize))
+    hash_sum = 0
+
+    for i in range(3):
+        channel = resized[:, :, i]
+        diff = channel[:, 1:] > channel[:, :-1]
+        s = sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
+        hash_sum += s
+
+    # convert boolen image to hash
+    return hash_sum
 
 
-"""
+
 img1 = cv2.imread('images/ey_boss.jpg')
+hash1 = diffHash(img1)
+
 img2 = cv2.imread('images/pink_guy.jpg')
-size = (160, 90)
-img1 = cv2.resize(img1, size)
-img2 = cv2.resize(img2, size)
+hash2 = diffHash(img2)
 
-phash1 = cv2.img_hash.pHash(img1)
-phash2 = cv2.img_hash.pHash(img2)
-print(phash1)
-print(phash2)
-
-phash1 = int.from_bytes(phash1.tobytes(), byteorder='big', signed=False)
-phash2 = int.from_bytes(phash2.tobytes(), byteorder='big', signed=False)
-
-print(phash1)
-print(phash2)
-
-
-
-print('difference: ' + str(phash1 - phash2))
-"""
+hash_diff = abs(hash1 - hash2)
+print(hash_diff)
