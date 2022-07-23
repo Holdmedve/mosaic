d6f9d8084598f6f5e7d7fff5d1e55b3062b4e936
Holdmedve <talos.boti@gmail.com>
2022-07-23 22:54:32 +0200

    ⬇️ Cut back on num frames to match

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def create_mosaic() -> Response:
             original_image_path=image_path,
             video_path=video_path,
             mosaic_tile_count=tile_count,
-            max_frames_to_match=1000,
+            max_frames_to_match=256,
         )
     )
     mosaic_file_name = f"{uuid.uuid1()}.jpg"
